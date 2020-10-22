1690e40436554385b16d9dcde537b76697ebe539
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-22 15:08:47 -0700

    setup initial structure for Hello.js

--- src/Hello.js
+++ src/Hello.js
@@ -1,1 +1,11 @@
 import React, { Component } from 'react';
+
+class Hello extends Component {
+    render() {
+        return (
+            <h1>
+                Hello World
+            </h1>
+        )
+    }
+}
