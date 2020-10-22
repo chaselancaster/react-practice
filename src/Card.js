0c751c8500228473d41268cabe8abc1c171580d1
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-22 15:42:27 -0700

    create initial card structure component

--- src/Card.js
+++ src/Card.js
@@ -1,1 +1,13 @@
 import React, { Component } from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src="" />
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    )
+}
