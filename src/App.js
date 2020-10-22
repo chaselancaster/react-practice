61228b41368f868409e03d5a1756a44a5b091756
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-22 15:07:19 -0700

    destructure component in app.js and change how class app is declared

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
