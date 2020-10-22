be46a94a814c1d2d2e410c3bc002d1a0a3cbc328
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-22 16:31:51 -0700

    restructure App.js so it is a class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import robots from './robots'
 
-const App = () => {
-    return (
-        <div>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div> 
-    )
+const state = {
+    robots: robots,
+    searchField: ''
+}
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>RoboFriends</h1>
+                <SearchBox />
+                <CardList robots={robots}/>
+            </div> 
+        )
+    } 
 }
 
 export default App;
