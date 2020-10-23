737b7b67e4081e4cce924e01b625f135cfa2aa37
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-22 22:05:38 -0700

    add onSearchChange functionality and filteredRobots const

--- src/App.js
+++ src/App.js
@@ -9,16 +9,20 @@ class App extends Component {
         searchField: ''
     }
 
-    onSearchChange(event) {
-        console.log(event)
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value })
+        console.log(event.target.value)
     }
 
     render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase)
+       })
         return (
             <div>
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={this.state.robots}/>
+                <CardList robots={filteredRobots}/>
             </div> 
         )
     } 
