72ae82b62fd4dcd72a81963a8f5e1fd9346d34c4
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-24 14:07:36 -0700

    import Scroll into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList'
 import SearchBox from './SearchBox'
+import Scroll from './Scroll'
 import { robots } from './robots'
 import './App.css'
 
