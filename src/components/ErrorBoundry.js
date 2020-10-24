6ad1e109ac77cf9051b92e6bdaf4419bc64df21f
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-24 15:18:42 -0700

    add componentDidCatch method to ErrorBoundry.js

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -6,10 +6,15 @@ class ErrorBoundry extends Component {
         hasError: false
     }
 
+    componentDidCatch(error, info) {
+        this.setState({ hasError: true })
+    }
+
     render() {
         if (this.state.hasError) {
             return <h1>Oooops. That is not good.</h1>
         }
+        return this.props.children
     }
 
 }
