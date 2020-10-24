8c98965011d44a2cd120740c5eb4d1c703e2c518
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-24 14:32:45 -0700

    remove if else statement and replace with ternary to display either loading h1 or render the components

--- src/containers/App.js
+++ src/containers/App.js
@@ -27,10 +27,9 @@ class App extends Component {
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase())
        })
-       if (!robots.length) {
-           return <h1>Loading</h1>
-       } else {
-            return (
+       return !robots.length ?
+        <h1>Loading</h1> :
+        (
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
@@ -41,6 +40,5 @@ class App extends Component {
             )
        }
     } 
-}
 
 export default App;
